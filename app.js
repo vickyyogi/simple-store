dc1606156f0f1200b992168c52366c454b738100
vickyyogi <pratamakyogi@gmail.com>
2026-06-14 11:58:50 +0700

    perubahan kecil

--- app.js
+++ app.js
@@ -8,27 +8,32 @@ renderProducts();
 
 function renderProducts() {
     productListElement.innerHTML = ""; // Clear existing products
-    const allProducts = daftarProduk.map(product => {
+    const allProducts = daftarProduk.map((product,index) => {
         const {id, namaProduk, kategori, harga, stok, isTersedia,
             varianWarna: [varian1, varian2],
             penilaian: { skor, jumlahUlasan},
             spesifikasi: { berat, kondisi }
          } = product;
-        return `
+
+         const showData = `
             <div class="card">
                 <h3>${namaProduk}</h3>
                 <p class="price">Price: ${harga.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) }</p>
                 <small>Kategori : ${kategori}</small>
                 <span class="rating">⭐${skor} , ${jumlahUlasan} ulasan</span>
-                <button class="btn-add">🛒 Add to Cart</button>
+                <button class="btn-add" onclick="addToCart(${index})">🛒 Add to Cart</button>
             </div>
 
         `;
+        return showData; 
     }).join("");
     productListElement.innerHTML = allProducts;
 }
 
 function addToCart(id){
-    const indexTarget = daftarProduk.findIndex((el,ind) => { ind === id});
+    const cartBadge = document.getElementById("cart-badge");
 
+    const indexTarget = daftarProduk.findIndex((el,ind) => { ind === id});
+    carts.push(indexTarget);
+    cartBadge.textContent = carts.length;
 }
