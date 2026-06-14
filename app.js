83cc1dd5f6ac1ea26777669973094bb5937b25c1
vickyyogi <pratamakyogi@gmail.com>
2026-06-14 14:31:37 +0700

    penambahan fitur cart , dan masih error

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ import daftarProduk from "./data.js";
 
 let carts = [];
 const productListElement = document.getElementById("product-list");
+const cartListElement = document.getElementById("showcart");
+const cartShow = document.getElementById("cart-show");
 
 // Menampilkan total produk di dalam array
 renderProducts();
+
+// Tambahkan event listener untuk semua tombol "Add to Cart"
+productListElement.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-add")) {
+        const id = parseInt(e.target.getAttribute("data-id"));
+        addToCart(id);
+    }
+});
 
 function renderProducts() {
     productListElement.innerHTML = ""; // Clear existing products
@@ -17,11 +27,11 @@ function renderProducts() {
 
          const showData = `
             <div class="card">
-                <h3>${namaProduk}</h3>
+                <h3><a href="#">${namaProduk}</a></h3>
                 <p class="price">Price: ${harga.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) }</p>
                 <small>Kategori : ${kategori}</small>
                 <span class="rating">⭐${skor} , ${jumlahUlasan} ulasan</span>
-                <button class="btn-add" onclick="addToCart(${index})">🛒 Add to Cart</button>
+                <button class="btn-add" data-id="${index}">🛒 Add to Cart</button>
             </div>
 
         `;
@@ -37,3 +47,24 @@ function addToCart(id){
     carts.push(indexTarget);
     cartBadge.textContent = carts.length;
 }
+
+//masih error, belum bisa menampilkan produk yang sudah di add to cart
+function renderCart(){
+    cartListElement.innerHTML = "";
+    const cartItems = carts.map((cartId) => {
+        const { namaProduk, harga } = daftarProduk[cartId];
+        return `
+            <li class="cart-item">
+                <span>${namaProduk}</span>
+                <span>${harga.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) }</span>
+            </li>
+        `;
+    }).join("");
+    cartListElement.innerHTML = cartItems;
+}
+
+cartShow.addEventListener("click", (e) => {
+    e.preventDefault();
+    cartListElement.classList.toggle("display-none");
+    renderCart();
+});
